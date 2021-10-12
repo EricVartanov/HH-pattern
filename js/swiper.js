3d2e587fdbcf4bae3be137def1fe2e06b7f998ae
EricVartanov <82024638+EricVartanov@users.noreply.github.com>
2021-10-12 23:00:35 +0300

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -5,9 +5,24 @@ const conditionsSwiper = new Swiper('.conditions__swiper', {
 
 
     // Default parameters
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
 
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 15
+        },
+        // when window width is >= 480px
+        576: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        // when window width is >= 640px
+
+    },
 
     // Navigation arrows
     navigation: {
